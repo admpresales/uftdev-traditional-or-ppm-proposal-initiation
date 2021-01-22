63baec50143effa0af8f24915b55489f7694bf95
Don Jackson <don.jackson@microfocus.com>
2021-01-22 19:10:07 +0000

    Initial commit into bare repo

---
+++ src/main/java/com/mf/UFTDeveloperTest.java
@@ -0,0 +1,48 @@
+package com.mf;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import com.hp.lft.sdk.*;
+import com.hp.lft.verifications.*;
+import com.hp.lft.sdk.web.*;
+
+import unittesting.*;
+
+public class UFTDeveloperTest extends UnitTestClassBase {
+    Browser browser;
+
+    public UFTDeveloperTest() {
+        //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        instance = new UFTDeveloperTest();
+        globalSetup(UFTDeveloperTest.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        globalTearDown();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        browser = BrowserFactory.launch(BrowserType.CHROME);
+        browser.clearCache();
+        browser.deleteCookies();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        browser.closeAllTabs();
+    }
+
+    @Test
+    public void test() throws GeneralLeanFtException {
+    }
+
+}
