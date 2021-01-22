9cc61916c7b28f4d0559be1bdf5ed5f5d2944373
Don Jackson <don.jackson@microfocus.com>
2021-01-22 19:42:48 +0000

    Commit of initial version of completed script.

--- src/main/java/com/mf/UFTDeveloperTest.java
+++ src/main/java/com/mf/UFTDeveloperTest.java
@@ -1,5 +1,8 @@
 package com.mf;
 
+import com.hp.lft.report.ReportException;
+import com.hp.lft.report.Reporter;
+import com.hp.lft.report.Status;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -10,6 +13,8 @@ import com.hp.lft.verifications.*;
 import com.hp.lft.sdk.web.*;
 
 import unittesting.*;
+
+import java.util.Date;
 
 public class UFTDeveloperTest extends UnitTestClassBase {
     Browser browser;
@@ -43,7 +48,74 @@ public class UFTDeveloperTest extends UnitTestClassBase {
 
     @Test
     public void test() throws GeneralLeanFtException {
+        java.util.Date date=new Date();
+        String ProposalName = "Proposal Name" + date;
+        String ExecutiveOverview = "This is the description for " + ProposalName;
+
         browser.navigate("http://nimbusserver.aos.com:8088");
+        browser.sync();
+
+        Image StrategicPortfolioImage = browser.describe(Image.class, new ImageDescription.Builder()
+                .src("http://nimbusserver.aos.com:8088/images/btn_PFM_up.png")
+                .type(com.hp.lft.sdk.web.ImageType.LINK).build());
+        StrategicPortfolioImage.click();
+        browser.sync();
+        Area TinaFryLoginLink = browser.describe(Area.class, new AreaDescription.Builder()
+                .href(new RegExpProperty(".*USERNAME=tfry.*")).build());
+        TinaFryLoginLink.click();
+        browser.sync();
+        Link CreateLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .tagName("A")
+                .innerText("CREATE").build());
+        CreateLink.click();
+        browser.sync();
+        Link ProposalLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .tagName("A")
+                .innerText("Proposal").build());
+        ProposalLink.click();
+        browser.sync();
+        ListBox BusinessUnitListBox = browser.describe(ListBox.class, new ListBoxDescription.Builder()
+                .title("Business Unit: is a Required Field.").build());
+        BusinessUnitListBox.select("Corporate");
+        browser.sync();
+        EditField ProposalNameField = browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .title(new RegExpProperty("Proposal Name:.*")).build());
+        ProposalNameField.setValue(ProposalName);
+        EditField ExecutiveOverviewField = browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .title(new RegExpProperty("Executive Overview:.*")).build());
+        ExecutiveOverviewField.setValue(ExecutiveOverview);
+        EditField BusinessObjectiveField = browser.describe(EditField.class, new EditFieldDescription.Builder()
+                .title(new RegExpProperty("Business Objective:.*")).build());
+        BusinessObjectiveField.setValue("9 Month Release Cycle");
+        Link SubmitLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .innerText("Submit").build());
+        SubmitLink.click();
+        browser.sync();
+        WebElement YourRequestIsCreatedWebElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .tagName("SPAN")
+                .innerText("Your Request is Created").build());
+        if (YourRequestIsCreatedWebElement.exists()){
+            try {
+                Reporter.reportEvent("Creation of Proposal","The proposal was created successfully", Status.Passed);
+            } catch (ReportException e) {
+                e.printStackTrace();
+            }
+
+        }
+        else {
+            try {
+                Reporter.reportEvent("Creation of Proposal","The Proposal created status text didn't display within the default timeout", Status.Failed);
+            } catch (ReportException e) {
+                e.printStackTrace();
+            }
+
+        }
+        WebElement MenuUserIconWebElement = browser.describe(WebElement.class, new WebElementDescription.Builder()
+                .id("menuUserIcon").build());
+        MenuUserIconWebElement.click();
+        Link SignOutLink = browser.describe(Link.class, new LinkDescription.Builder()
+                .innerText(new RegExpProperty("Sign Out.*")).build());
+        SignOutLink.click();
         browser.sync();
     }
 
